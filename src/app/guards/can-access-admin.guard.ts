b2d3388d9ef832a3f3d05cf4ee2aeb204d7260e5
truongph17848 <truongndph17848@fpt.edu.vn>
2022-06-22 21:15:43 +0700

    update phân quyền , role =- ,chuyển về trang chính

--- src/app/guards/can-access-admin.guard.ts
+++ src/app/guards/can-access-admin.guard.ts
@@ -24,7 +24,7 @@ export class CanAccessAdminGuard implements CanActivate {
     }
 }
     
-    this.router.navigateByUrl('/auth/login');
+    this.router.navigateByUrl('/');
     return false
   }
   
